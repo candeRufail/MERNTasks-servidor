191c5d4c870efe5553c37f77ce34e1d0a5cc449a
unknown <candelariarufail@gmail.com>
2020-07-20 12:16:32 -0300

    php

---
+++ index.php
@@ -0,0 +1,31 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+//CREAR EL SERVIDOR!!!!!!!!
+
+const app = express();
+
+//conectar a la base de datos
+conectarDB();
+
+app.use(cors());
+
+//habilitar express.json
+app.use(express.json({ extended: true }));
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/tareas', require('./routes/tareas'));
+
+
+//arrancar la app
+app.listen(PORT, '0.0.0.0',  () => {
+    console.log(`el servidor esta funcionando en el puerto ${PORT}`);
+});
+
+//holaaaaaaaaaaaa
